92729f9676f70d346a5913c88e6a9a140cd147c3
Erik Chancellor <erik@Chancellor.localdomain>
2024-03-30 13:54:21 -0600

    updated apexcharts import

--- app/components/Holdings.js
+++ app/components/Holdings.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
-import Chart from 'react-apexcharts'
+import dynamic from "next/dynamic"
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false })
 
 const Holdings = ({ tokens }) => {
 	const defaultSymbols = ["---", "---", "---", "---", "---"]
@@ -50,7 +51,8 @@ const Holdings = ({ tokens }) => {
           }}
           series={balances ? balances : defaultBalances}
           type="pie"
-          height={300}
+          height="300"
+          width="100%"
         />
 
       </div>
